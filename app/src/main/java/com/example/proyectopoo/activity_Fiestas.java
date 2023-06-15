c5b33f3a6bd5ddb20eabbfe72e5a93f0113178c3
Jonathan Steve Rosero Garcia <steve.rosero03@outlook.com>
2023-06-15 18:37:28 -0500

    PANTALLAS

---
+++ app/src/main/java/com/example/proyectopoo/activity_Fiestas.java
@@ -0,0 +1,16 @@
+package com.example.proyectopoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_Fiestas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fiestas);
+    }
+
+
+}
--- app/src/main/java/com/example/proyectopoo/activity_preguntas.java
+++ app/src/main/java/com/example/proyectopoo/activity_preguntas.java
@@ -2,7 +2,9 @@ package com.example.proyectopoo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_preguntas extends AppCompatActivity {
 
@@ -11,4 +13,21 @@ public class activity_preguntas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas);
     }
+
+    public void vistaFiesta(View activity_Fiestas){
+        Intent fiesta = new Intent(this, activity_Fiestas .class);
+        startActivity(fiesta);
+    }
+
+    public void vistaNaturaleza(View activity_naturaleza){
+        Intent naturaleza = new Intent(this, activity_naturaleza.class);
+        startActivity(naturaleza);
+    }
+
+    public void vistaHistoria(View activity_historia){
+        Intent historia = new Intent(this, activity_historia.class);
+        startActivity(historia);
+    }
+
+
 }
